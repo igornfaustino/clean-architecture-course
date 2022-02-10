6dd6bdb67fc7ffe2f10fdd4e40194edeb9d250fe
igornfaustino <igor@nfaustino.com>
2022-02-10 12:48:56 -0300

    add cpf class

---
+++ src/cpf.test.ts
@@ -0,0 +1,16 @@
+import { CPF } from "./cpf";
+
+test.each(["111.111.111-11", "123.456.789-99"])(
+  "should return false for an invalid cpf: %s",
+  (cpf) => {
+    expect(() => new CPF(cpf)).toThrow();
+  }
+);
+
+test("should return true for an valid cpf", () => {
+  expect(new CPF("935.411.347-80").cpf).toBe("93541134780");
+});
+
+test("should return false if no cpf is passed", () => {
+  expect(() => new CPF("")).toThrow();
+});
---
+++ src/cpf.ts
@@ -0,0 +1,40 @@
+export class CPF {
+  public cpf: string;
+
+  constructor(cpf: string) {
+    const normalizedCpf = this.normalizeCpf(cpf);
+    if (!this.isCPFValid(normalizedCpf)) throw new Error("CPF is invalid");
+    this.cpf = normalizedCpf;
+  }
+
+  private isEveryDigitTheSame = (digits: string) =>
+    digits.split("").every((c) => c === digits![0]);
+
+  private calcVerificationNumber(digits: string) {
+    let multiplier = digits.length + 1;
+    const multipliedDigits = digits
+      .split("")
+      .map((digit) => parseInt(digit) * multiplier--);
+    const digitsSum = multipliedDigits.reduce((acc, curr) => acc + curr, 0);
+    const rest = digitsSum % 11;
+    return rest < 2 ? 0 : 11 - rest;
+  }
+
+  private calculateCPF(cpf: string) {
+    const firstNineDigits = cpf.substring(0, 9);
+    const firstVerificationDigit = this.calcVerificationNumber(firstNineDigits);
+    const firstTenDigits = `${firstNineDigits}${firstVerificationDigit}`;
+    const secondVerificationDigit = this.calcVerificationNumber(firstTenDigits);
+    return `${firstTenDigits}${secondVerificationDigit}`;
+  }
+
+  private normalizeCpf = (cpf: string) => cpf.replace(/[\D]/g, "");
+
+  private isCPFValid(cpf: string | null | undefined) {
+    if (!cpf) return false;
+    if (cpf.length !== 11) return false;
+    if (this.isEveryDigitTheSame(cpf)) return false;
+    const calculatedCpf = this.calculateCPF(cpf);
+    return cpf == calculatedCpf;
+  }
+}
