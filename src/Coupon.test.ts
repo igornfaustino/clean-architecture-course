e9691ae3ae5ecc3d1b00a5271cf1b8ec4ad693e5
igornfaustino <igor@nfaustino.com>
2022-02-20 18:27:55 -0300

    ensure coupon is invalid when current time is expire time

--- src/Coupon.test.ts
+++ src/Coupon.test.ts
@@ -14,6 +14,13 @@ describe("Coupon", () => {
     expect(coupon.isValid).toBe(true);
   });
 
+  test("should be valid if now is the same as expireDate", () => {
+    const coupon = new Coupon("ANY_NAME", 20, new Date("2022-02-20"));
+    jest.useFakeTimers().setSystemTime(new Date("2020-02-20"));
+
+    expect(coupon.isValid).toBe(true);
+  });
+
   test("should be invalid if now is after expireDate", () => {
     const coupon = new Coupon("ANY_NAME", 20, new Date("2022-02-20"));
     jest.useFakeTimers().setSystemTime(new Date("2022-02-21"));
