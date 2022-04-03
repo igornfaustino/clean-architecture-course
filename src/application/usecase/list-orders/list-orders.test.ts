2ed6ef73487272d71ffe3eb8a77ee2c2b4b5d5c8
igornfaustino <igor@nfaustino.com>
2022-04-03 20:25:05 -0300

    change list orders output

--- src/application/usecase/list-orders/list-orders.test.ts
+++ src/application/usecase/list-orders/list-orders.test.ts
@@ -1,12 +1,12 @@
 import { Order } from "../../../domain/entity/Order"
-import OrderRepositoryMemory from "../../../infra/repository/memory/OrderRepositoryMemory"
+import { MemoryRepositoryFactory } from "../../../infra/factory/MemoryRepositoryFactory"
 import { ListOrders } from "./list-orders"
 
 test('should get a list of orders', async () => {
-  const orderRepository = new OrderRepositoryMemory()
-  const listOrder = new ListOrders(orderRepository)
-  orderRepository.save(new Order('46996189870', 1, new Date('2021-10-21')))
-  orderRepository.save(new Order('46996189870', 2, new Date('2021-10-21')))
+  const repositoryFactory = new MemoryRepositoryFactory()
+  const listOrder = new ListOrders(repositoryFactory)
+  listOrder.orderRepository.save(new Order('46996189870', 1, new Date('2021-10-21')))
+  listOrder.orderRepository.save(new Order('46996189870', 2, new Date('2021-10-21')))
 
   const orders = await listOrder.execute()
 
--- src/application/usecase/list-orders/list-orders.ts
+++ src/application/usecase/list-orders/list-orders.ts
@@ -1,9 +1,29 @@
+import RepositoryFactory from "../../../domain/factory/RepositoryFactory";
+import { ItemRepository } from "../../../domain/repository/ItemRepository";
 import { OrderRepository } from "../../../domain/repository/OrderRepository";
+import { GetOrderOutput } from "../get-order-by-code/GetOrderByCodeOutput";
 
 export class ListOrders {
-  constructor(private readonly orderRepository: OrderRepository) { }
+  orderRepository: OrderRepository;
+  itemRepository: ItemRepository;
+
+  constructor(readonly repositoryFactory: RepositoryFactory) {
+    this.orderRepository = repositoryFactory.createOrderRepository();
+    this.itemRepository = repositoryFactory.createItemRepository();
+  }
 
   async execute() {
-    return this.orderRepository.getAll()
+    const orders = await this.orderRepository.getAll()
+    const output: GetOrderOutput[] = [];
+    for (const order of orders) {
+      const items: { description: string, price: number }[] = [];
+      for (const orderItem of order.items) {
+        const item = await this.itemRepository.getById(orderItem.idItem);
+        if (!item) throw new Error("Item not found");
+        items.push({ description: item.description, price: orderItem.price });
+      }
+      output.push(new GetOrderOutput(order.code.value, order.getTotal(), items));
+    }
+    return output;
   }
 }
